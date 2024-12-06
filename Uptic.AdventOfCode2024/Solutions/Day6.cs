b1828c139011b8940042197a86975263bf10cf1f
Cees-JanNolen <cees-jan.nolen@uptic.io>
2024-12-06 14:43:12 +0100

    part B day 6

--- Uptic.AdventOfCode2024/Solutions/Day6.cs
+++ Uptic.AdventOfCode2024/Solutions/Day6.cs
@@ -4,8 +4,6 @@ public class Day6 : IDay
 {
     public string PartA(List<string> lines)
     {
-        var ans = 0;
-
         var matrix = new char[lines.Count, lines.First().Length];
 
         var startPosition = (0, 0);
@@ -22,18 +20,14 @@ public class Day6 : IDay
             }
         }
 
-        ans = GuardWalk(matrix, startPosition);
-
-
-        return ans.ToString();
+        return GuardWalkPath(matrix, startPosition).Count.ToString();
     }
 
-    private static int GuardWalk(char[,] matrix, (int, int) startPosition)
+    private static HashSet<(int, int)> GuardWalkPath(char[,] matrix, (int, int) startPosition)
     {
         var currentPos = startPosition;
         var currentDirection = Direction.N;
-        var visitedPositions = new HashSet<(int, int)>();
-        visitedPositions.Add(currentPos);
+        var visitedPositions = new HashSet<(int, int)> { (currentPos) };
         while (true)
         {
             var nextPos = GetNextPosition(currentPos, currentDirection);
@@ -54,21 +48,7 @@ public class Day6 : IDay
             }
         }
 
-        // foreach (var visitedPosition in visitedPositions)
-        // {
-        //     matrix[visitedPosition.Item1, visitedPosition.Item2] = 'X';    
-        // }
-        // for (var y = 0; y < matrix.GetLength(0); y++)
-        // {
-        //     for (var i = 0; i < matrix.GetLength(1); i++)
-        //     {
-        //         Console.Write(matrix[y, i]);
-        //     }
-        //
-        //     Console.WriteLine();
-        // }
-
-        return visitedPositions.Count;
+        return visitedPositions;
     }
 
     private static (int, int) GetNextPosition((int, int) currentPosition, Direction direction)
@@ -96,8 +76,65 @@ public class Day6 : IDay
     public string PartB(List<string> lines)
     {
         var ans = 0;
+        var matrix = new char[lines.Count, lines.First().Length];
+
+        var startPosition = (0, 0);
+        for (var i = 0; i < lines.Count; i++)
+        {
+            var line = lines[i];
+            for (var j = 0; j < line.Length; j++)
+            {
+                matrix[i, j] = line[j];
+                if (line[j].Equals('^'))
+                {
+                    startPosition = (i, j);
+                }
+            }
+        }
+
+        var firstPath = GuardWalkPath(matrix, startPosition).Distinct().ToList();
+        
+        foreach (var valueTuple in firstPath)
+        {
+            var oldChar = matrix[valueTuple.Item1, valueTuple.Item2];
+            matrix[valueTuple.Item1, valueTuple.Item2] = '0';
+            ans += TryInfiniteLoop(matrix, startPosition) ? 1 : 0;
+            matrix[valueTuple.Item1, valueTuple.Item2] = oldChar;
+        }
 
         return ans.ToString();
+    }
+    
+    private static bool TryInfiniteLoop(char[,] matrix, (int, int) startPosition)
+    {
+        var currentPos = startPosition;
+        var currentDirection = Direction.N;
+        var visitedPositions = new HashSet<((int, int), Direction)> { (currentPos, currentDirection) };
+        while (true)
+        {
+            var nextPos = GetNextPosition(currentPos, currentDirection);
+            try
+            {
+                if (matrix[nextPos.Item1, nextPos.Item2] == '#' || matrix[nextPos.Item1, nextPos.Item2] == '0')
+                {
+                    currentDirection = (Direction)(((int)currentDirection + 1) % 4);
+                    continue;
+                }
+
+                currentPos = nextPos;
+                var canAdd = visitedPositions.Add((currentPos, currentDirection));
+                if (!canAdd)
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                break;
+            }
+        }
+
+        return false;
     }
 
     private enum Direction
